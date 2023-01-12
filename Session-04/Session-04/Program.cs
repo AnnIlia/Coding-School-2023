44039646fd615896a0bd7fdd3e54b408dc695e37
Anna Ilia <annailia9802@gmail.com>
2023-01-12 15:46:12 +0200

    Create Programm2

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -5,3 +5,13 @@ string name = "Anna";
 
 Console.WriteLine(hello + " " + name);
 
+
+// Programm Two
+int numberA = 6;
+int numberB = 3;
+
+int Sum = numberA + numberB;
+int Division = numberA / numberB;
+
+Console.WriteLine( Sum + "     " + Division);
+Console.ReadLine();
