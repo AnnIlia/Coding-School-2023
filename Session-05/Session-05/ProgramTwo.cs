eaf0b6a3a54a70781f3792181d8b0b17391ca38c
Anna Ilia <annailia9802@gmail.com>
2023-01-13 16:17:31 +0200

    create ProgramTwo and had some tries

---
+++ Session-05/Session-05/ProgramTwo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramOne
+{
+    class SumProduct
+    {
+        static void Main(int[] args)
+        {
+            
+            Console.WriteLine("Enter a number");
+
+            string givenNumber = Console.ReadLine();
+            // string reverse = "";
+            int number = Convert.ToInt32(givenNumber);
+
+            
+
+            
+            
+
+            Console.WriteLine($"Your revesed name is {number}");
+            Console.ReadLine();
+        }
+    }
+}
+}
