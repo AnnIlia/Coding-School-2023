cce7f141556261560bf61d1b4207184ffbbb7370
Anna Ilia <annailia9802@gmail.com>
2023-01-15 16:54:15 +0200

    complete Project5

---
+++ Session-05/Session-05/ProgramFive.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class ProgramFive
+    {
+        public static void Main(string[] args)
+        {
+
+            int[] array1 = new int[] { 0, -2, 1, 20, -31, 50, -4, 17, 89, 100 };
+            int temp;
+            // traverse 0 to array length
+            for (int i = 0; i < array1.Length - 1; i++)
+            {
+
+                // traverse i+1 to array length
+                for (int j = i + 1; j < array1.Length; j++)
+                {
+
+                    // compare array element with
+                    // all next element
+                    if (array1[i] > array1[j])
+                    {
+
+                        temp = array1[i];
+                        array1[i] = array1[j];
+                        array1[j] = temp;
+                    }
+                }
+            }
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.WriteLine(array1[i]);   
+            }
+        }
+
+    }
+}
