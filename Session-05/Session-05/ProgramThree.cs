61885a860463f12a1ffdd9b285a8d49e29530643
Anna Ilia <annailia9802@gmail.com>
2023-01-15 16:18:55 +0200

    complete ProgramThtree

--- Session-05/Session-05/ProgramThree.cs
+++ Session-05/Session-05/ProgramThree.cs
@@ -8,5 +8,39 @@ namespace Session_05
 {
     internal class ProgramThree
     {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            int i = 0;
+            int num = 0;
+
+            for (num = 1; num <= n; num++)
+            {
+                count = 0;
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                    else {
+                        //Console.Write("else\n");
+                    }
+                    
+                }
+                
+                if (count == 0 && num != 1)
+                {
+                    Console.Write(num + " \n" );
+                }
+
+            }
+
+        }
+
+        }
     }
-}
+
