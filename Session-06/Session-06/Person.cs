b4c977cfb25eb4fe82f1e64f8645649a92273ac9
Anna Ilia <annailia9802@gmail.com>
2023-01-16 15:13:21 +0200

    add method at Person

--- Session-06/Session-06/Person.cs
+++ Session-06/Session-06/Person.cs
@@ -11,7 +11,15 @@ namespace Session_06
         public int Age { get; set; }
 
         // methods
+        public void GetName()
+        {
 
+        }
+
+        public void SetName(string name)
+        {
+
+        }
 
     }
 }
