11297d8b33a7ce171fa750e5c5251ee5f39f0b0c
Anna Ilia <annailia9802@gmail.com>
2023-01-16 16:15:00 +0200

    add properties & methods at Institute

--- Session-06/Session-06/Institute.cs
+++ Session-06/Session-06/Institute.cs
@@ -1,8 +1,25 @@
 ﻿using System;
 
-public class Class1
+namespace Session_06
 {
-	public Class1()
-	{
-	}
+
+    public class Institute
+    {
+        // properties
+        public Guid ID { get; set; }
+        public string? Name { get; set; }
+        public int YearsInService { get; set; }
+
+        // methods
+        public void GetName()
+        {
+
+        }
+
+        public void SetName(string name)
+        {
+
+        }
+
+    }
 }
