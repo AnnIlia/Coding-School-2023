b14850f509a73db549eae432d481ad7c34926f8e
Anna Ilia <annailia9802@gmail.com>
2023-01-16 15:02:51 +0200

    add properties at Grade. ***TODO: fix int grade

--- Session-06/Session-06/Grade.cs
+++ Session-06/Session-06/Grade.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 
-public class Class1
+namespace Session_06
 {
-	public Class1()
-	{
-	}
+
+    public class Grade
+    {
+        // properties
+        public Guid ID { get; set; }
+        public Guid StudentID { get; set; }
+        public Guid CourseID { get; set; }
+
+        public int grade { get; set; }
+
+    }
 }
