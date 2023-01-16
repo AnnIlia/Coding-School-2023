5308ee803ccb23cc6ab1a9a03af01b022ec73303
Anna Ilia <annailia9802@gmail.com>
2023-01-16 16:48:31 +0200

    add properties and methods at University.

--- Session-06/Session-06/University.cs
+++ Session-06/Session-06/University.cs
@@ -1,3 +1,38 @@
 ﻿using System;
+using System.Globalization;
+
+namespace Session_06
+{
+
+    public class University
+    {
+        // properties
+        public Student[]? students { get; set; }
+        public Course[]? courses { get; set; }
+        public Grade[]? grades { get; set; }
+        public Schedule[]? ScheduleCourse { get; set; }
 
 
+        // methods
+        public void GetStudents()
+        {
+
+        }
+
+        public void GetCourses()
+        {
+
+        }
+
+        public void GetGrades()
+        {
+
+        }
+
+        public void SetSchedule(Guid courseID, Guid ProfessorID, DateTime datetime)
+        {
+
+        }
+
+    }
+}
