092f0524f003f164bb8343fc93d37b87f4b2b4a6
Anna Ilia <annailia9802@gmail.com>
2023-01-17 17:23:35 +0200

    convert/ try

--- Session-07/Session-07/Exercise07.cs
+++ Session-07/Session-07/Exercise07.cs
@@ -1,10 +1,11 @@
 ﻿using Session_07;
 using System;
+using System.Xml.Linq;
 
 namespace Session_07
 {
- //define class diagram
-   
+    //define class diagram
+
     public enum ActionEnum
     {
         Convert,
@@ -12,12 +13,12 @@ namespace Session_07
         Reverse
     }
 
-    public class Message 
+    public class Message
     {
         // properties
-        public Guid RequestID { get; set; }
+        public Guid? RequestID { get; set; }
         public DateTime TimeStamp { get; set; }
-        public String Message  { get; set; }
+        public String Message { get; set; }
         //no method
     }
 
@@ -25,24 +26,27 @@ namespace Session_07
     {
         // properties
         public Message[] Messages { get; set; }
-       
-        Message[] Messages = new Message[1000]
+
+        Message[] Messages = new Message[1000];
 
 
         // methods
         public ReadAll()
         {
             //do
+            return
         }
-       
+
         public ClearAll()
         {
             //do
+            return
         }
-        
+
         public Write(Message)
         {
             //do
+            return 
         }
 
     }
@@ -53,40 +57,94 @@ namespace Session_07
         public Guid RequestID { get; set; }
         public string Input { get; set; }
         public ActionEnum Action { get; set; }
+
+        // methods (request4 from exercise)
+
+        public double implementation(ShapeType shapeType, double radius, double side)
+        {
+
+            double surface = 0;
+
+            switch (shapeType)
+            {
+                case ShapeType.Circle:
+                    surface = SurfaceCircle(radius);
+                    break;
+
+                case ShapeType.Square:
+                    surface = SurfaceSquare(side);
+                    break;
+
+                case ShapeType.Triangle:
+                    surface = SurfaceTriangle(side);
+                    break;
+
+                default:
+                    Console.WriteLine("please select another type");
+                    break;
+            }
+
+            return surface;
+
+        }
+
+
+
+
+
+
+
+
     }
+
 
     public class ActionResponse
     {
         // properties
         public Guid RequestID { get; set; }
-        public Guid ResponsetID { get; set;
+        public Guid ResponsetID { get; set; }
         public string Output { get; set; }
     }
-}
 
     public class ActionResolver : ActionRequest
     {
 
-        // PROPERTIES
+        // properties
         public MessageLogger Logger { get; set; }
 
-        // CTOR
+        // Constructor
         public ActionResolver(Guid requestId)
         {
-        RequestID guid = requestId;
+            RequestID guid = requestId;
         }
 
-        // METHODS
+        // methods
         public ActionResponse Execute(ActionRequest request)
         {
 
 
             return null;
         }
-    */
-
-   
-}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
 
---
+++ Session-07/Session-07/ProgramS07.cs
@@ -0,0 +1,113 @@
+﻿using Session_07;
+using System;
+
+namespace Session_07
+{
+ //define class diagram
+   
+    public enum ActionEnum
+    {
+        Convert,
+        Uppercase,
+        Reverse
+    }
+
+    public class Message 
+    {
+        // properties
+        public Guid RequestID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public String Message  { get; set; }
+        //no method
+    }
+
+    public class MessageLogger : Message
+    {
+        // properties
+        public Message[] Messages { get; set; }
+       
+        Message[] Messages = new Message[1000]
+
+
+        // methods
+        public ReadAll()
+        {
+            //do
+        }
+       
+        public ClearAll()
+        {
+            //do
+        }
+        
+        public Write(Message)
+        {
+            //do
+        }
+
+    }
+
+    public class ActionRequest
+    {
+        // properties
+        public Guid RequestID { get; set; }
+        public string Input { get; set; }
+        public ActionEnum Action { get; set; }
+
+        // methods (request4 from exercise)
+
+
+
+    }
+
+
+    public class ActionResponse
+    {
+        // properties
+        public Guid RequestID { get; set; }
+        public Guid ResponsetID { get; set;
+        public string Output { get; set; }
+    }
+
+    public class ActionResolver : ActionRequest
+    {
+
+        // properties
+        public MessageLogger Logger { get; set; }
+
+        // Constructor
+        public ActionResolver(Guid requestId)
+        {
+            RequestID guid = requestId;
+        }
+
+        // methods
+        public ActionResponse Execute(ActionRequest request)
+        {
+
+
+            return null;
+        }
+    
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
