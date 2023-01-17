6a648b6257dcd3f6ab36112a03d3e290f58176af
Anna Ilia <annailia9802@gmail.com>
2023-01-17 14:40:08 +0200

    create cs. and add classes and some properties of them.

---
+++ Session-07/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Session_07
+{
+ //define class diagram
+   
+    public enum ActionEnum
+    {
+        Convert,
+        Uppercase,
+        Reverse
+    }
+
+    public class Message 
+    {
+        // properties
+        public Guid RequestID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public String Message  { get; set; }
+    }
+
+    public class MessageLogger
+    {
+        // properties
+        public Message[] Messages { get; set; } 
+        
+    }
+
+    public class ActionRequest
+    {
+        // properties
+        public Guid RequestID { get; set; }
+        public string Input { get; set; }
+        public ActionEnum Action { get; set; }
+    }
+
+    public class ActionResponse
+    {
+
+    }
+
+    public class ActionResolver
+    {
+
+        // PROPERTIES
+        public MessageLogger Logger { get; set; }
+
+        // CTOR
+        public ActionResolver()
+        {
+
+        }
+
+        // METHODS
+        public ActionResponse Execute(ActionRequest request)
+        {
+
+
+            return null;
+        }
+    */
+
+   
+}
+
+
+
