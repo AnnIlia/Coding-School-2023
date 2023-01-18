2539cd8e6d70cd49cf6feac7f9c6e6000d68e932
Anna Ilia <annailia9802@gmail.com>
2023-01-18 15:17:55 +0200

    paste others

--- Session-07NEW/Session-07NEW/Program.cs
+++ Session-07NEW/Session-07NEW/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+
+using Session_07;
+
+ActionRequest request = new ActionRequest()
+{
+    Input = "Fotis",
+    Action = ActionEnum.Reverse
+};
+
+ActionResponse response = new ActionResponse();
+
+ActionResolver resolver = new ActionResolver();
+
+response = resolver.Execute(request);
+
+// DISPLAY MESSAGES FROM LOGGER
+
+resolver.Logger.ReadAll();
+
+Console.ReadLine();
+
--- Session-07NEW/Session-07NEW/StringParser.cs
+++ Session-07NEW/Session-07NEW/StringParser.cs
@@ -1,8 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Class1
+namespace Session_07
 {
-	public Class1()
-	{
-	}
+
+
+    public class StringManipulator
+    {
+
+        public string Text { get; set; }
+
+        public virtual string Manipulate()
+        {
+
+            return string.Empty;
+        }
+
+    }
+
+    public class StringConvert : StringManipulator
+    {
+
+
+        public override string Manipulate()
+        {
+
+            // “Convert” you must check if the Input is a decimal number and convert it to binary.
+            // ...
+
+
+            return string.Empty;
+        }
+    }
+
+    public class StringReverse : StringManipulator
+    {
+
+        public override string Manipulate()
+        {
+
+            return string.Empty;
+        }
+
+    }
+
+    public class StringUppercase : StringManipulator
+    {
+
+        public override string Manipulate()
+        {
+
+            return string.Empty;
+        }
+    }
+
 }
