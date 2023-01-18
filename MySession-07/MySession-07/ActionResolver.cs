809666d2d46a5e314876b71e73ac8423962dbae7
Anna Ilia <annailia9802@gmail.com>
2023-01-18 23:58:05 +0200

    add .cs of Message

--- MySession-07/MySession-07/ActionResolver.cs
+++ MySession-07/MySession-07/ActionResolver.cs
@@ -17,26 +17,7 @@ namespace MySession_07
         Reverse
     }
 
-    public class Message
-    {
-        //properties
-        public Guid ID { get; set; }
-        public DateTime TimeStamp { get; set; }
-        //Message renamed as MessageText > cannot be the same as class name
-        public string MessageText { get; set; }
-
-        //constructor
-        public Message()
-        {
-            ID = Guid.NewGuid();        
-        }
-        public Message(string text)
-        {
-            ID= Guid.NewGuid();
-            TimeStamp= DateTime.Now;
-            MessageText = text;
-        }
-    }
+   
 
     public class MessageLogger
     {
---
+++ MySession-07/MySession-07/Message.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MySession_07
+{
+    public class Message
+    {
+        //properties
+        public Guid ID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        //Message renamed as MessageText > cannot be the same as class name
+        public string MessageText { get; set; }
+
+        //constructor
+        public Message()
+        {
+            ID = Guid.NewGuid();
+        }
+        public Message(string text)
+        {
+            ID = Guid.NewGuid();
+            TimeStamp = DateTime.Now;
+            MessageText = text;
+        }
+    }
+
+}
