3bb81b1a54d8dc0fa1f811ef957c03cc96913318
Anna Ilia <annailia9802@gmail.com>
2023-01-18 23:59:20 +0200

    add .cs of MessageLogger

---
+++ MySession-07/MySession-07/MessageLogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace MySession_07
+{
+    public class MessageLogger
+    {
+        //properties
+        public Message[] Messages { get; set; }
+
+        //constructor
+        public MessageLogger()
+        {
+            Messages = new Message[1000];
+        }
+        //methods 
+        public void ReadAll()
+        {
+
+        }
+
+        public void Clear()
+        {
+
+        }
+
+        public void Write(Message message)
+        {
+
+        }
+
+    }
