2b0519891b490fe6522a4e48865732d5e0f2ff7e
Anna Ilia <annailia9802@gmail.com>
2023-01-18 18:00:39 +0200

    add properties at objects

--- MySession-07/MySession-07/ActionResover.cs
+++ MySession-07/MySession-07/ActionResover.cs
@@ -34,22 +34,35 @@ namespace MySession_07 {
 
 public class Message
     {
+        //properties
+        public Guid ID { get; set; }    
+        public DateTime TimeStamp { get; set; } 
+        //Message renamed as MessageText
+        public string MessageText { get; set; }
 
     }
 
  public class MessageLogger 
     {
-    
+        //properties
+    public Message[] Messages { get; set; }
+
     }
 
 public class ActionRequest
     {
-
+        //properties
+        public Guid RequestID { get; set; }
+        public Guid ResponseID { get; set; }
+        public string Output { get; set; }      
     } 
 
 public class ActionResponse
     {
-
+        //properties
+        public Guid RequestID { set; get; }
+        public Guid ResponseID { set; get; }
+        public string Output { get; set; }
     }
 
 
