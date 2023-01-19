e41062f2e6a41a91f5b1bf35ccb29cd1bb3fcdc9
Anna Ilia <annailia9802@gmail.com>
2023-01-19 16:36:25 +0200

    create raise to power

--- Session-09/CalculatorLib/RaiseToPower.cs
+++ Session-09/CalculatorLib/RaiseToPower.cs
@@ -1,8 +1,21 @@
-﻿using System;
-
-public class Class1
+﻿namespace CalculatorLib
 {
-	public Class1()
-	{
-	}
-}
+
+
+    public class RaiseToPower
+    {
+
+        public decimal Do(decimal? x, decimal? y)
+        {
+
+            decimal ret = 0;
+
+            if (x != null && y != null)
+            {
+            //    double ret = Math.Pow(double x.Value,double y.Value);
+            }
+
+            return ret;
+        }
+
+    }
