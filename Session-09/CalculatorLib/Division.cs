0b7c4418013e7a28bc43bf72d37409711a93bc8f
Anna Ilia <annailia9802@gmail.com>
2023-01-19 17:45:22 +0200

    fix divition

--- Session-09/CalculatorLib/Division.cs
+++ Session-09/CalculatorLib/Division.cs
@@ -2,17 +2,19 @@
 {
 
 
-    public class Division
+    public class Divition
     {
 
         public decimal Do(decimal? x, decimal? y)
         {
+
 
             decimal ret = 0;
 
             if (x != null && y != null)
             {
                 ret = x.Value / y.Value;
+
             }
 
             return ret;
