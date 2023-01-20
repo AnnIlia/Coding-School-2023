ec6c65dc9bc48b828a5d8039aa43811762be9191
Anna Ilia <annailia9802@gmail.com>
2023-01-20 16:44:19 +0200

    add  Schedule.cs

--- NewSession-10/UniLibrary/UniLibrary/University.cs
+++ NewSession-10/UniLibrary/UniLibrary/University.cs
@@ -1,7 +1,59 @@
-﻿namespace UniLibrary
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniLibrary
 {
-    public class Class1
+
+
+    public class Institute
     {
+
+        public Guid ID { get; set; }
+        public string? Name { get; set; }
+        public int YearsInService { get; set; }
+
+
+        public Institute()
+        {
+            ID = Guid.NewGuid();
+        }
+    }
+
+    public class University : Institute
+    {
+
+
+        public List<Student> Students { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Grade> Grades { get; set; }
+        public List<Schedule> Schedules { get; set; }
+
+
+
+
+
+        public University()
+        { 
+            Students = new List<Student>();
+            
+            Courses = new List<Course>();   
+            Grades = new List<Grade>();
+            Schedules = new List<Schedule>(); 
+             
+
+        }
+
+    }
+
+    public class Student
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
 
     }
 }
