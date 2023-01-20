d33d3c9e3f55f8268c5bbfe89591acca848e929a
Anna Ilia <annailia9802@gmail.com>
2023-01-20 16:41:02 +0200

    add schedule.cs

---
+++ NewSession-10/UniLibrary/UniLibrary/Schedule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniLibrary
+{
+    public class Schedule
+    {
+        // properties
+        public Guid ID { get; set; }
+        public Guid CourseID { get; set; }
+        public Guid ProfessorID { get; set; }
+        public DateTime Callendar { get; set; }
+
+    }
+}
