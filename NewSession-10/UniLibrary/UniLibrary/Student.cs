cf76980ed4727dbebce61d608f49b4d1df08ec14
Anna Ilia <annailia9802@gmail.com>
2023-01-25 01:07:08 +0200

    create helpers/some examples

---
+++ NewSession-10/UniLibrary/UniLibrary/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniLibrary
+{
+
+    public class Student : Person
+    {
+        // properties
+        public int RegistrationNumber { get; set; }
+        public Course[]? courses { get; set; }
+
+
+        // methods
+        public void Attend(string Course, DateTime dateTime)
+        {
+
+        }
+
+        public void WriteExam(string Course, DateTime dateTime)
+        {
+
+        }
+    }
+
+}
+
