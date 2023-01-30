33cb078e8013f8818f6e23abc5049469f4f443dd
Anna Ilia <annailia9802@gmail.com>
2023-01-30 15:46:50 +0200

    add 2 libraries and some classes of today's session

---
+++ EF.Course.Model/EF.Course.Model/Todo.cs
@@ -0,0 +1,13 @@
+﻿namespace EF.Course.Model
+{
+    public class Todo
+    {
+        public Todo(string title) {
+        Title = title;
+        }
+
+        public string Title { get; set; }
+        public bool Finished { get; set; }
+
+    }
+}
