257baed76160dc8f4a76f8fd2fd7a4fbc03ce40a
Anna Ilia <annailia9802@gmail.com>
2023-01-30 14:01:28 +0200

    created test-1 for extra Practice

---
+++ Test1/Test1/Student.cs
@@ -0,0 +1,22 @@
+﻿Stuusing System;
+
+
+
+namespase Test1
+{
+    public class Student
+    {
+    public Student
+    {
+        public Guid ID { get; set; }
+        public enum GenderEnum { Male, Female, Other}
+        public string Name { get; set; } 
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public bool Undergraduate { get; set; }
+        public GenderEnum Gender { get; set; }
+
+
+    }
+}
+}
