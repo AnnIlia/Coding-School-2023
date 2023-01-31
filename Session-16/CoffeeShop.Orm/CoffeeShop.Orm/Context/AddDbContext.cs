f98476ca1a7cb00df15a1df34649b79e095db384
Anna Ilia <annailia9802@gmail.com>
2023-01-31 17:51:16 +0200

    create AddDbContext.cs

---
+++ Session-16/CoffeeShop.Orm/CoffeeShop.Orm/Context/AddDbContext.cs
@@ -0,0 +1,40 @@
+﻿using CoffeeShop.Model;
+using CoffeeShop.Orm.Configuration;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Orm.Context
+{
+    public class AddDbContext : DbContext
+    {
+        //maybe must include and Customer
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<TransactionLine> TransactionLines { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+            modelBuilder.ApplyConfiguration(new ProductCatConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionLineConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TodoDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+    }
+}
