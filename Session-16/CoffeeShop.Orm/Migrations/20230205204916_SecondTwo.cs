1936fff7619350afcaff7cbf3d9be61d5abdbe68
Anna Ilia <annailia9802@gmail.com>
2023-02-05 22:51:52 +0200

    finally run migrations

---
+++ Session-16/CoffeeShop.Orm/Migrations/20230205204916_SecondTwo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CoffeeShop.Orm.Migrations
+{
+    /// <inheritdoc />
+    public partial class SecondTwo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
