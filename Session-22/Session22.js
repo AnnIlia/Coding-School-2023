a54a76e8547032b923f7de253cd5569fdf155b44
Anna Ilia <annailia9802@gmail.com>
2023-02-07 14:07:35 +0200

    semi-completed 1

--- Session-22/Session22.js
+++ Session-22/Session22.js
@@ -1,1 +1,22 @@
-// JavaScript source code
+//Session22
+
+// 1. JavaScript function to reverse a string
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+// UI code to accept input and display result
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+const result = document.querySelector(".result");
+let reversed = reverseString("lala");
+
+// Log the result to the console
+console.log(reversed);
+btn.addEventListener("click", function () {
+    result.textContent = reverseString(input.value);
+});
+
+
+// // Log the result to the console
+console.log(reversed);
