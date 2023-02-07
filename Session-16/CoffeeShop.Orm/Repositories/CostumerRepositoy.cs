5c3d59c41addd0ae19a744419f1a4bd8b25e6230
Anna Ilia <annailia9802@gmail.com>
2023-02-08 00:10:40 +0200

    ProductCategoryRepository completed (?)

--- Session-16/CoffeeShop.Orm/Repositories/CostumerRepositoy.cs
+++ Session-16/CoffeeShop.Orm/Repositories/CostumerRepositoy.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using CoffeeShop.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CoffeeShop.Orm.Context;
+using CoffeeShop.Orm.Repositories;
 
-namespace CoffeeShop.Orm.Repositories
+namespace CoffeeShop.EF.Repositories
 {
-    internal class Class1
-    {
-    }
+        public class CustomerRepository : IEntityRepository<Customer>
+        {
+            public void Add(Customer ent)
+            {
+                using var context = new AddDbContext();
+                context.Customer.Add(ent);
+                context.SaveChanges();
+            }
+            public void Delete(Guid id)
+            {
+                using var context = new AddDbContext();
+                var tempCus = context.Customers.SingleOrDefault(cus => cus.ID == id);
+                if (tempCus is null)
+                    return;
+                context.Customers.Remove(tempCus);
+                context.SaveChanges();
+            }
+            public List<Customer> GetAll()
+            {
+                using var context = new AddDbContext();
+                return context.Customers.ToList();
+            }
+            public Customer? GetById(Guid id)
+            {
+                using var context = new AddDbContext();
+                return context.Customers.Where(cus => cus.ID == id).SingleOrDefault();
+            }
+            public void Update(Guid id, Customer ent)
+            {
+                using var context = new AddDbContext();
+                var tempCus = context.Customers.SingleOrDefault(cus => cus.ID == id);
+                if (tempCus is null)
+                    return;
+                tempCus.Code = ent.Code;
+                tempCus.Description = ent.Description;
+                tempCus.Transaction = ent.Transaction;
+                context.SaveChanges();
+            }
+        }
 }
