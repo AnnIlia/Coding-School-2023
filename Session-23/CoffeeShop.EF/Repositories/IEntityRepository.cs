47d7e41b1ae1af8cb570181bb223fbf67a29e88d
Anna Ilia <annailia9802@gmail.com>
2023-02-10 13:54:13 +0200

    : change class with BaseEntity @IEntityRepository

--- Session-23/CoffeeShop.EF/Repositories/IEntityRepository.cs
+++ Session-23/CoffeeShop.EF/Repositories/IEntityRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+﻿using CoffeeShop.Model;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.EntityFrameworkCore.Update.Internal;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,7 @@ using System.Threading.Tasks;
 namespace CoffeeShop.EF.Repositories
 {
     public interface IEntityRepository<IEntity>
-        where IEntity : class
+        where IEntity : BaseEntity
     {
         IList<IEntity> GetAll();
 
