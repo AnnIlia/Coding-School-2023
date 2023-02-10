4992f74b4d040fd6da4c1cbdc5406c6404dab972
Anna Ilia <annailia9802@gmail.com>
2023-02-10 16:12:21 +0200

    TransactionLineRepository completed

--- Session-23/CoffeeShop.EF/Repositories/TransactionLineRepository.cs
+++ Session-23/CoffeeShop.EF/Repositories/TransactionLineRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CoffeeShop.EF.Context;
+using CoffeeShop.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,58 @@ using System.Threading.Tasks;
 
 namespace CoffeeShop.EF.Repositories
 {
-    internal class TransactionLineRepository
+    public class TransactionLineRepository : IEntityRepository<TransactionLine>
+
+
     {
+        public void Create(TransactionLine entity)
+        {
+            using var context = new CoffeeShopDbContext();
+            context.TransactionLines.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            using var context = new CoffeeShopDbContext();
+            var foundTransactionLine = context.TransactionLines.SingleOrDefault(TransactionLine => TransactionLine.Id == id);
+            if (foundTransactionLine is null)
+                return;
+
+            context.TransactionLines.Remove(foundTransactionLine);
+            context.SaveChanges();
+        }
+
+        public IList<TransactionLine> GetAll()
+        {
+            using var context = new CoffeeShopDbContext();
+            return context.TransactionLines.ToList();
+        }
+
+        public TransactionLine? GetById(int id)
+        {
+            using var context = new CoffeeShopDbContext();
+            return context.TransactionLines.Where(TransactionLine => TransactionLine.Id == id).SingleOrDefault();
+        }
+
+        public void Update(int id, TransactionLine entity)
+        {
+            using var context = new CoffeeShopDbContext();
+            var foundTransactionLine = context.TransactionLines.SingleOrDefault(TransactionLine => TransactionLine.Id == id);
+            if (foundTransactionLine is null)
+                return;
+
+            foundTransactionLine.Quantity= entity.Quantity;
+            foundTransactionLine.Price = entity.Price;
+            foundTransactionLine.Product= entity.Product;
+            foundTransactionLine.ProductId= entity.ProductId;
+            foundTransactionLine.Discount= entity.Discount;
+            foundTransactionLine.TotalPrice = entity.TotalPrice;
+
+            context.SaveChanges();
+        }
+
     }
+   
 }
+
