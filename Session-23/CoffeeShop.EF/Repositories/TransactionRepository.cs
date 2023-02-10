4a0972c516641672eb48c7b929d021b89a232bfc
Anna Ilia <annailia9802@gmail.com>
2023-02-10 16:25:13 +0200

    TransactionRepository: completed

--- Session-23/CoffeeShop.EF/Repositories/TransactionRepository.cs
+++ Session-23/CoffeeShop.EF/Repositories/TransactionRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CoffeeShop.EF.Context;
+using CoffeeShop.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,53 @@ using System.Threading.Tasks;
 
 namespace CoffeeShop.EF.Repositories
 {
-    internal class TransactionRepository
+    public class TransactionRepository : IEntityRepository<Transaction>
+
+
     {
+        public void Create(Transaction entity)
+        {
+            using var context = new CoffeeShopDbContext();
+            context.Transactions.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            using var context = new CoffeeShopDbContext();
+            var foundTransaction = context.Transactions.SingleOrDefault(transaction => transaction.Id == id);
+            if (foundTransaction is null)
+                return;
+            context.Transactions.Remove(foundTransaction);
+            context.SaveChanges();
+        }
+
+        public IList<Transaction> GetAll()
+        {
+            using var context = new CoffeeShopDbContext();
+            return context.Transactions.ToList();
+        }
+
+        public Transaction? GetById(int id)
+        {
+            using var context = new CoffeeShopDbContext();
+            return context.Transactions.Where(transaction => transaction.Id == id).SingleOrDefault();
+        }
+
+        public void Update(int id, Transaction entity)
+        {
+            using var context = new CoffeeShopDbContext();
+            var foundTransaction = context.Transactions.SingleOrDefault();  
+            if (foundTransaction is null) 
+                return;
+
+            foundTransaction.Date = entity.Date;
+            foundTransaction.EmployeeId = entity.EmployeeId;
+            foundTransaction.CustomerId = entity.CustomerId;
+            foundTransaction.PaymentMethod = entity.PaymentMethod;
+            foundTransaction.TotalPrice = entity.TotalPrice;
+            
+            context.SaveChanges();
+        }
     }
 }
