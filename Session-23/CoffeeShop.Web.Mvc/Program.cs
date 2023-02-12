2057001bbaa2a5d0a22e7a9d4d001d0ad2cd7a14
Anna Ilia <annailia9802@gmail.com>
2023-02-12 17:06:43 +0200

    continue with errors. Build Logic

--- Session-23/CoffeeShop.Web.Mvc/Program.cs
+++ Session-23/CoffeeShop.Web.Mvc/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// -------PROBLEM
 //builder.Services.AddScoped<IEntityRepo<ProductCategory>, ProductCategoryRepository>();
 
 var app = builder.Build();
