9828b54b52206e771f7e87cfacfa3b6493371ca5
Anna Ilia <annailia9802@gmail.com>
2023-02-18 21:10:56 +0200

    Employee class created

--- Session-30/FuelStation.Model/Employee.cs
+++ Session-30/FuelStation.Model/Employee.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿namespace FuelStation.Model
 
-public class Class1
+public class Employee
 {
-	public Class1()
-	{
-	}
+    public Employee(string name, string surname, int salaryPerMonth, EmployeeType employeeType)
+    {
+        Name = name;
+        Surname = surname;
+        SalaryPerMonth = salaryPerMonth;
+        EmployeeType = employeeType;
+
+        Transactions = new List<Transaction>();
+    }
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public DateTime HireDateStart { get; set; }
+    public DateTime HireDateEnd { get; set; }
+    public int SalaryPerMonth { get; set; }
+    public EmployeeType EmployeeType { get; set; }
+
+    // Relations
+    public List<Transaction> Transactions { get; set; }
 }
