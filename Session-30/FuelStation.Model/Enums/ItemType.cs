4b74023d715de22417741f5101ba6699c86f3041
Anna Ilia <annailia9802@gmail.com>
2023-02-19 15:13:30 +0200

    added all entities - model project

---
+++ Session-30/FuelStation.Model/Enums/ItemType.cs
@@ -0,0 +1,14 @@
+﻿namespace FuelStation.Model.Enums
+{
+    public class ItemType
+    {
+        public enum ItemType
+        {
+            Fuel = 1,
+            Product = 2,
+            Service = 3
+        }
+    }
+}
+
+
---
+++ Session-30/FuelStation.Model/Enums/PaymentMethod.cs
@@ -0,0 +1,10 @@
+﻿namespace FuelStation.Enums
+
+public class PaymentMethod
+{
+	public enum PaymentMethod()
+	{
+		Cash = 1,
+        CreditCard = 2
+	}
+}
