f18ec8ca3d929af3200eb5473c413641f17a71b4
Anna Ilia <annailia9802@gmail.com>
2023-02-21 00:32:03 +0200

    IEntityRepo re-created as inerface item

---
+++ Session-30/FuelStation.EF/Repositories/IEntityRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuelStation.EF.Repositories
+{
+    public interface IEntityRepository<TEntity>
+    {
+        IList<TEntity> GetAll();
+        TEntity? GetById(int Id);
+        void Add(TEntity entity);
+        void Update(int Id, TEntity entity);
+        void Delete(int Id);
+    }
+}
