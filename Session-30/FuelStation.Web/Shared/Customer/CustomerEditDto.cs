a8b4a6775e1f3aff5716ddcf041040e2ac85dabf
Anna Ilia <annailia9802@gmail.com>
2023-02-22 00:24:35 +0200

    customer corrected

--- Session-30/FuelStation.Web/Shared/Customer/CustomerEditDto.cs
+++ Session-30/FuelStation.Web/Shared/Customer/CustomerEditDto.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using FuelStation.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Transactions;
+
 
 namespace FuelStation.Web.Shared.Customer
 {
