78bbe0f6ea1b95ecfdf186cd0a1800c509cdbf1e
Anna Ilia <annailia9802@gmail.com>
2023-02-21 21:20:40 +0200

    CustomerContoller - create, list, edit created - todo: changes

---
+++ Session-30/FuelStation.Web/Server/Controllers/CustomerController.cs
@@ -0,0 +1,82 @@
+﻿using FuelStation.EF.Repositories;
+using FuelStation.Web.Shared.Customer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FuelStation.Model;
+using FuelStation.Web.Shared;
+
+namespace FuelStation.Web.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly IEntityRepo<Customer> _customerRepo;
+        public CustomerController(IEntityRepo<Customer> customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+
+        //Index - GetAlL()
+        [HttpGet]
+        public async Task<IEnumerable<CustomerListDto>> Get()
+        {
+            var customerList = _customerRepo.GetAll();
+            return customerList.Select(customer => new CustomerListDto
+            {
+                Id = customer.Id,
+                Name = customer.Name,
+                Surname = customer.Surname,
+                CardNumber = customer.CardNumber
+            });
+        }
+
+        //Create - Post
+        [HttpPost]
+        public async Task Post(CustomerCreateDto customer)
+        {
+            var newCustomer = new Customer(customer.Name, customer.Surname, customer.CardNumber);
+            newCustomer.Transactions = new();
+            _customerRepo.Add(newCustomer);
+        }
+
+
+        //Delete
+        [HttpDelete("{Id}")]
+        public async Task Delete(int id)
+        {
+            _customerRepo.Delete(id);
+        }
+
+        //Edit - GetbyID
+        [HttpGet("{Id}")]
+        public async Task<CustomerEditDto> GetById(int id)
+        {
+            var result = _customerRepo.GetById(id);
+            return new CustomerEditDto
+            {
+                Id = id,
+                Name = result.Name,
+                Surname = result.Surname,
+                CardNumber = result.CardNumber    
+                
+            };
+        }
+
+
+        //Put
+        public async Task Put(CustomerEditDto customer)
+        {
+            var itemToUpdate = _customerRepo.GetById(customer.Id);
+            itemToUpdate.Id = customer.Id;
+            itemToUpdate.Name = customer.Name;
+            itemToUpdate.Surname = customer.Surname;
+            itemToUpdate.CardNumber = customer.CardNumber;
+           // itemToUpdate.Transactions = customer.Transactions;
+            _customerRepo.Update(customer.Id, itemToUpdate);
+        }
+
+
+
+    }
+}
---
+++ Session-30/FuelStation.Web/Shared/Customer/CustomerEditDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace FuelStation.Web.Shared.Customer
+{
+    public class CustomerEditDto
+    {
+        public int Id { get; set; }
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "The Name field can only contain Latin letters ")]
+        [Required]
+        public string Name { get; set; }
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "The Surname field can only contain Latin letters ")]
+        [Required]
+        public string Surname { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format("{0} {1}", Name, Surname);
+            }
+        }
+        [RegularExpression(@"^A\d+$", ErrorMessage = "Card number must start with 'A' and contain only digits")]
+        [Required]
+        public string CardNumber { get; set; }
+
+        // Relations
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+    }
+}
