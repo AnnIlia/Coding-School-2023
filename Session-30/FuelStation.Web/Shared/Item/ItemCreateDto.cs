b0bbde157dc962ed85637d104208f51e15bdfce2
Anna Ilia <annailia9802@gmail.com>
2023-02-22 15:10:18 +0200

    ItemCreate created *todo:validation -price/cost

--- Session-30/FuelStation.Web/Shared/Item/ItemCreateDto.cs
+++ Session-30/FuelStation.Web/Shared/Item/ItemCreateDto.cs
@@ -13,10 +13,19 @@ namespace FuelStation.Web.Shared.Item
     //mono gia tin create
     public class ItemCreateDto
     {
+        
+        [RegularExpression("^[a-zA-Z0-9]+$", ErrorMessage = "The Code field can contain Latin letters and digits ")]
+        [Required]
         public string Code { get; set; }
+        [RegularExpression("^[a-zA-Z]*$", ErrorMessage = "The Description field can only contain Latin letters ")]
+        [Required]
         public string Description { get; set; }
-        public ItemType ItemType { get; set; }
+        public ItemType ItemType { get; set; } 
+        //[RegularExpression(@"^[1-9][0-9]* (\.[0-9]+)?$", ErrorMessage = "The Price field can only digits")]
+        //[Required]
         public decimal Price { get; set; }
+        //[RegularExpression(@"^[1-9][0-9]* (\.[0-9]+)?$", ErrorMessage = "The Cost field can only digits")]
+        //[Required]
         public decimal Cost { get; set; }
 
         //Relations
