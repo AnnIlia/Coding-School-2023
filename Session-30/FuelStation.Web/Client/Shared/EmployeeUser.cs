e7c751eae567a74f47ae10d3658647a16720d2b9
Anna Ilia <annailia9802@gmail.com>
2023-02-23 23:51:38 +0200

    employeeuser class created

---
+++ Session-30/FuelStation.Web/Client/Shared/EmployeeUser.cs
@@ -0,0 +1,11 @@
+﻿using FuelStation.Enums;
+using FuelStation.Model;
+
+namespace Session_30.Client.Shared
+{
+    public class EmployeeUser
+    {
+
+        public EmployeeType? Role { get; set; }
+    }
+}
