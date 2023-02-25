bca33ed010c6675ff136a478f198b594dd7dbda5
Anna Ilia <annailia9802@gmail.com>
2023-02-25 04:32:04 +0200

    ItemForm mini change

--- Session-30/FuelStation.Win/Items.Designer.cs
+++ Session-30/FuelStation.Win/Items.Designer.cs
@@ -147,6 +147,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btnNewItem);
             this.Controls.Add(this.btnClose);
