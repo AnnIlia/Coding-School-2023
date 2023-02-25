aa65a9b02a1fea9df72dd80108032b9f8dc51d34
Anna Ilia <annailia9802@gmail.com>
2023-02-25 05:08:45 +0200

    changed ui - login

--- Session-30/FuelStation.Win/Login.Designer.cs
+++ Session-30/FuelStation.Win/Login.Designer.cs
@@ -28,17 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
             this.btnLgManager = new System.Windows.Forms.Button();
             this.btnLgCashier = new System.Windows.Forms.Button();
             this.btnLgStaff = new System.Windows.Forms.Button();
             this.lbFuelStation = new System.Windows.Forms.Label();
             this.lbLoginAs = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnLgManager
             // 
             this.btnLgManager.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnLgManager.Location = new System.Drawing.Point(87, 233);
+            this.btnLgManager.Location = new System.Drawing.Point(95, 408);
             this.btnLgManager.Name = "btnLgManager";
             this.btnLgManager.Size = new System.Drawing.Size(177, 67);
             this.btnLgManager.TabIndex = 0;
@@ -49,7 +52,7 @@
             // btnLgCashier
             // 
             this.btnLgCashier.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnLgCashier.Location = new System.Drawing.Point(313, 233);
+            this.btnLgCashier.Location = new System.Drawing.Point(321, 408);
             this.btnLgCashier.Name = "btnLgCashier";
             this.btnLgCashier.Size = new System.Drawing.Size(177, 67);
             this.btnLgCashier.TabIndex = 1;
@@ -60,7 +63,7 @@
             // btnLgStaff
             // 
             this.btnLgStaff.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnLgStaff.Location = new System.Drawing.Point(532, 233);
+            this.btnLgStaff.Location = new System.Drawing.Point(540, 408);
             this.btnLgStaff.Name = "btnLgStaff";
             this.btnLgStaff.Size = new System.Drawing.Size(177, 67);
             this.btnLgStaff.TabIndex = 2;
@@ -72,7 +75,7 @@
             // 
             this.lbFuelStation.AutoSize = true;
             this.lbFuelStation.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbFuelStation.Location = new System.Drawing.Point(221, 69);
+            this.lbFuelStation.Location = new System.Drawing.Point(229, 244);
             this.lbFuelStation.Name = "lbFuelStation";
             this.lbFuelStation.Size = new System.Drawing.Size(367, 41);
             this.lbFuelStation.TabIndex = 3;
@@ -81,19 +84,30 @@
             // lbLoginAs
             // 
             this.lbLoginAs.AutoSize = true;
-            this.lbLoginAs.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbLoginAs.Location = new System.Drawing.Point(352, 154);
+            this.lbLoginAs.Font = new System.Drawing.Font("Yu Gothic UI Light", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.lbLoginAs.Location = new System.Drawing.Point(290, 321);
             this.lbLoginAs.Name = "lbLoginAs";
-            this.lbLoginAs.Size = new System.Drawing.Size(87, 28);
+            this.lbLoginAs.Size = new System.Drawing.Size(217, 31);
             this.lbLoginAs.TabIndex = 4;
-            this.lbLoginAs.Text = "Login As";
+            this.lbLoginAs.Text = "------ Login As ------";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(306, 65);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(192, 140);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(800, 419);
+            this.ClientSize = new System.Drawing.Size(800, 567);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.lbLoginAs);
             this.Controls.Add(this.lbFuelStation);
             this.Controls.Add(this.btnLgStaff);
@@ -101,6 +115,7 @@
             this.Controls.Add(this.btnLgManager);
             this.Name = "Login";
             this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -113,5 +128,6 @@
         private Button btnLgStaff;
         private Label lbFuelStation;
         private Label lbLoginAs;
+        private PictureBox pictureBox1;
     }
 }
--- Session-30/FuelStation.Win/Login.cs
+++ Session-30/FuelStation.Win/Login.cs
@@ -9,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace FuelStation.Win
 {
     public partial class Login : Form
@@ -21,15 +22,27 @@ namespace FuelStation.Win
             InitializeComponent();
 
         }
+        public FuelStationApp()
+        {
+            InitializeComponent();
 
+        }
         private void btnLgCashier_Click(object sender, EventArgs e)
         {
+            _role = EmployeeType.Cashier;
+            Menu menu = new Menu(_role);
+            menu.Show();
 
+            this.Hide();
         }
 
         private void btnLgStaff_Click(object sender, EventArgs e)
         {
-            
+            _role = EmployeeType.Staff;
+            Menu menu = new Menu(_role);
+            menu.Show();
+
+            this.Hide();
         }
 
         private void btnLgManager_Click(object sender, EventArgs e)
