e025a2b9dfb0444dd26a75a7f18ee31f7ea46264
Anna Ilia <annailia9802@gmail.com>
2023-02-25 14:24:14 +0200

    menuForm completed

--- Session-30/FuelStation.Win/Menu.cs
+++ Session-30/FuelStation.Win/Menu.cs
@@ -1,4 +1,5 @@
 ﻿using FuelStation.Enums;
+using FuelStation.Model;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -16,70 +17,81 @@ namespace FuelStation.Win
         private EmployeeType _role;
         public Menu(EmployeeType role)
         {
-            InitializeComponent();
             _role = role;
-            
+            InitializeComponent();
+            this.StartPosition = FormStartPosition.CenterScreen;
+            RoleAccessView();
+        }
+
+        private void RoleAccessView()
+        {
+            switch (_role)
+            {
+                case EmployeeType.Manager:
+
+                    btnMnCustomers.Visible = true;
+                    btnMnTransactions.Visible = true;
+                    btnMnItems.Visible = true;
+
+                    break;
+                case EmployeeType.Cashier:
+
+                    btnMnCustomers.Visible = false;
+                    btnMnTransactions.Visible = true;
+                    btnMnItems.Visible = true;
+
+                    break;
+                case EmployeeType.Staff:
+
+                    btnMnCustomers.Visible = false;
+                    btnMnTransactions.Visible = false;
+                    btnMnItems.Visible = true;
+
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void ManagerForm_Load(object sender, EventArgs e)
+        {
+
         }
         private void btnMnCustomers_Click(object sender, EventArgs e)
         {
-
+            var customersForm = new Customers();
+            customersForm.ShowDialog();
+            this.Close();
         }
 
         private void btnClose_Click(object sender, EventArgs e)
         {
-            
-      
+            Login loginForm = new Login();
+            loginForm.Show();
+            this.Close();
+
         }
 
         private void btnMnTransactions_Click(object sender, EventArgs e)
         {
-            
-            
+            var trasForm = new Transaction();
+            trasForm.ShowDialog();
+            this.Close();
+
         }
 
         private void btnMnItems_Click(object sender, EventArgs e)
         {
-           
+            var itemFrom = new Items();
+            itemFrom.ShowDialog();
+            this.Close();
         }
     }
-
-
-    private void RoleBasedView()
-    {
-        switch (_role)
-        {
-            case EmployeeType.Manager:
-
-               
-
-                break;
-            case EmployeeType.Cashier:
-
-               
-
-                break;
-            case EmployeeType.Staff:
-
-                
-
-                break;
-            default:
-                break;
-        }
-    }
-
-
-
-
-
-
-
-
-
-
-
-
 }
+
+
+
+ 
 
 
 
