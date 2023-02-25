400354b0d33b0f2596fb9bb22a97ee6659b6b8e6
Anna Ilia <annailia9802@gmail.com>
2023-02-25 04:30:18 +0200

    ItemsForm created

---
+++ Session-30/FuelStation.Win/Items.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace FuelStation.Win
+{
+    partial class Items
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ItemCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemCost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ItemPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lbItems = new System.Windows.Forms.Label();
+            this.btnNewItem = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ItemCode,
+            this.ItemType,
+            this.ItemDescription,
+            this.ItemCost,
+            this.ItemPrice});
+            this.dataGridView1.Location = new System.Drawing.Point(60, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(683, 202);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // ItemCode
+            // 
+            this.ItemCode.HeaderText = "Code";
+            this.ItemCode.MinimumWidth = 6;
+            this.ItemCode.Name = "ItemCode";
+            this.ItemCode.Width = 125;
+            // 
+            // ItemType
+            // 
+            this.ItemType.HeaderText = "Type";
+            this.ItemType.MinimumWidth = 6;
+            this.ItemType.Name = "ItemType";
+            this.ItemType.Width = 125;
+            // 
+            // ItemDescription
+            // 
+            this.ItemDescription.HeaderText = "Description";
+            this.ItemDescription.MinimumWidth = 6;
+            this.ItemDescription.Name = "ItemDescription";
+            this.ItemDescription.Width = 125;
+            // 
+            // ItemCost
+            // 
+            this.ItemCost.HeaderText = "Cost";
+            this.ItemCost.MinimumWidth = 6;
+            this.ItemCost.Name = "ItemCost";
+            this.ItemCost.Width = 125;
+            // 
+            // ItemPrice
+            // 
+            this.ItemPrice.HeaderText = "Price";
+            this.ItemPrice.MinimumWidth = 6;
+            this.ItemPrice.Name = "ItemPrice";
+            this.ItemPrice.Width = 125;
+            // 
+            // lbItems
+            // 
+            this.lbItems.AutoSize = true;
+            this.lbItems.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbItems.Location = new System.Drawing.Point(333, 39);
+            this.lbItems.Name = "lbItems";
+            this.lbItems.Size = new System.Drawing.Size(167, 28);
+            this.lbItems.TabIndex = 5;
+            this.lbItems.Text = "Items Information";
+            // 
+            // btnNewItem
+            // 
+            this.btnNewItem.Font = new System.Drawing.Font("Yu Gothic UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnNewItem.Location = new System.Drawing.Point(263, 320);
+            this.btnNewItem.Name = "btnNewItem";
+            this.btnNewItem.Size = new System.Drawing.Size(299, 42);
+            this.btnNewItem.TabIndex = 9;
+            this.btnNewItem.Text = "Add New Item";
+            this.btnNewItem.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Yu Gothic UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnClose.Location = new System.Drawing.Point(521, 384);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(137, 42);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Yu Gothic UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDelete.Location = new System.Drawing.Point(321, 384);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(137, 42);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Yu Gothic UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSave.Location = new System.Drawing.Point(127, 384);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(137, 42);
+            this.btnSave.TabIndex = 6;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // Items
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnNewItem);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lbItems);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Items";
+            this.Text = "Items";
+            this.Load += new System.EventHandler(this.Items_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn ItemCode;
+        private DataGridViewTextBoxColumn ItemType;
+        private DataGridViewTextBoxColumn ItemDescription;
+        private DataGridViewTextBoxColumn ItemCost;
+        private DataGridViewTextBoxColumn ItemPrice;
+        private Label lbItems;
+        private Button btnNewItem;
+        private Button btnClose;
+        private Button btnDelete;
+        private Button btnSave;
+    }
+}
