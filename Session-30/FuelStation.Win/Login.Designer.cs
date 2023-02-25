dc2aa45df3b29582711f771792e22013c7ddcdb8
Anna Ilia <annailia9802@gmail.com>
2023-02-25 03:30:51 +0200

    LoginForm formated

--- Session-30/FuelStation.Win/Login.Designer.cs
+++ Session-30/FuelStation.Win/Login.Designer.cs
@@ -37,15 +37,18 @@
             // 
             // btnLgManager
             // 
+            this.btnLgManager.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnLgManager.Location = new System.Drawing.Point(87, 233);
             this.btnLgManager.Name = "btnLgManager";
             this.btnLgManager.Size = new System.Drawing.Size(177, 67);
             this.btnLgManager.TabIndex = 0;
             this.btnLgManager.Text = "Manager";
             this.btnLgManager.UseVisualStyleBackColor = true;
+            this.btnLgManager.Click += new System.EventHandler(this.btnLgManager_Click);
             // 
             // btnLgCashier
             // 
+            this.btnLgCashier.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnLgCashier.Location = new System.Drawing.Point(313, 233);
             this.btnLgCashier.Name = "btnLgCashier";
             this.btnLgCashier.Size = new System.Drawing.Size(177, 67);
@@ -56,28 +59,32 @@
             // 
             // btnLgStaff
             // 
+            this.btnLgStaff.Font = new System.Drawing.Font("Yu Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.btnLgStaff.Location = new System.Drawing.Point(532, 233);
             this.btnLgStaff.Name = "btnLgStaff";
             this.btnLgStaff.Size = new System.Drawing.Size(177, 67);
             this.btnLgStaff.TabIndex = 2;
             this.btnLgStaff.Text = "Staff";
             this.btnLgStaff.UseVisualStyleBackColor = true;
+            this.btnLgStaff.Click += new System.EventHandler(this.btnLgStaff_Click);
             // 
             // lbFuelStation
             // 
             this.lbFuelStation.AutoSize = true;
-            this.lbFuelStation.Location = new System.Drawing.Point(313, 80);
+            this.lbFuelStation.Font = new System.Drawing.Font("Yu Gothic UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbFuelStation.Location = new System.Drawing.Point(221, 69);
             this.lbFuelStation.Name = "lbFuelStation";
-            this.lbFuelStation.Size = new System.Drawing.Size(184, 20);
+            this.lbFuelStation.Size = new System.Drawing.Size(367, 41);
             this.lbFuelStation.TabIndex = 3;
             this.lbFuelStation.Text = "Fuel Station Windows App";
             // 
             // lbLoginAs
             // 
             this.lbLoginAs.AutoSize = true;
-            this.lbLoginAs.Location = new System.Drawing.Point(370, 151);
+            this.lbLoginAs.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbLoginAs.Location = new System.Drawing.Point(352, 154);
             this.lbLoginAs.Name = "lbLoginAs";
-            this.lbLoginAs.Size = new System.Drawing.Size(66, 20);
+            this.lbLoginAs.Size = new System.Drawing.Size(87, 28);
             this.lbLoginAs.TabIndex = 4;
             this.lbLoginAs.Text = "Login As";
             // 
@@ -85,7 +92,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 419);
             this.Controls.Add(this.lbLoginAs);
             this.Controls.Add(this.lbFuelStation);
             this.Controls.Add(this.btnLgStaff);
--- Session-30/FuelStation.Win/Login.cs
+++ Session-30/FuelStation.Win/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FuelStation.Enums;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,32 @@ namespace FuelStation.Win
 {
     public partial class Login : Form
     {
+        private EmployeeType _role;
+
+
         public Login()
         {
             InitializeComponent();
+
         }
 
         private void btnLgCashier_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnLgStaff_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnLgManager_Click(object sender, EventArgs e)
+        {
+            _role = EmployeeType.Manager;
+            Menu menu = new Menu(_role);
+            menu.Show();
+
+            this.Hide();
         }
     }
 }
